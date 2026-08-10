1a473f897b55d08d324d9ba82aec154f281dfe19
Matthew Lee <4513875+mattdeform@users.noreply.github.com>
2026-08-10 12:21:44 +1200

    Test session scoping, env vars pushed into GITHUB_ENV, and formatting fail now fails SA.

--- tests/maya/conftest.py
+++ tests/maya/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def run_in_maya():
     from maya import (
         standalone,
@@ -8,3 +8,5 @@ def run_in_maya():
     )
     standalone.initialize()
     cmds.scriptEditorInfo(e=True, sw=True, se=True, si=True)
+    yield
+    standalone.uninitialize()
